c1ab67af0aabddb838ab8ba30a5ce6565454468a
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-08-31 17:26:13 +0200

    Add project files.

---
+++ BikeListRazor/Model/Bike.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeListRazor.Model
+{
+    public class Bike
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Brand { get; set; }
+    }
+}
